8e095fac2eced30f6f9483e7d595ebcfeb844307
Maxim <maksimfhmad@gmail.com>
2025-03-06 11:09:30 +0100

    created array of cars and mapped them

--- app/cars/page.tsx
+++ app/cars/page.tsx
@@ -4,21 +4,106 @@ import { Car } from "../lib/types/types";
 
 type Props = {};
 
-const car: Car = {
-  name: "Super Car",
-  type: "BMW",
-  color: "green",
-  motor: {
-    serialNumber: 1234,
-    hp: 200,
+const cars: Car[] = [
+  {
+    name: "Speedster X",
+    type: "Audi",
+    color: "red",
+    motor: {
+      serialNumber: 5678,
+      hp: 250,
+    },
   },
-};
+  {
+    name: "Thunderbolt",
+    type: "Mercedes",
+    color: "black",
+    motor: {
+      serialNumber: 9101,
+      hp: 300,
+    },
+  },
+  {
+    name: "Road King",
+    type: "Ford",
+    color: "blue",
+    motor: {
+      serialNumber: 1121,
+      hp: 220,
+    },
+  },
+  {
+    name: "Shadow Racer",
+    type: "Tesla",
+    color: "silver",
+    motor: {
+      serialNumber: 3141,
+      hp: 400,
+    },
+  },
+  {
+    name: "Eco Drive",
+    type: "Toyota",
+    color: "white",
+    motor: {
+      serialNumber: 5161,
+      hp: 180,
+    },
+  },
+  {
+    name: "Beast Mode",
+    type: "Dodge",
+    color: "orange",
+    motor: {
+      serialNumber: 7181,
+      hp: 500,
+    },
+  },
+  {
+    name: "Skyline GTR",
+    type: "Nissan",
+    color: "gray",
+    motor: {
+      serialNumber: 9202,
+      hp: 350,
+    },
+  },
+  {
+    name: "Turbo Charge",
+    type: "Porsche",
+    color: "yellow",
+    motor: {
+      serialNumber: 1022,
+      hp: 450,
+    },
+  },
+  {
+    name: "Night Fury",
+    type: "Lamborghini",
+    color: "purple",
+    motor: {
+      serialNumber: 1224,
+      hp: 600,
+    },
+  },
+  {
+    name: "Hyper Sonic",
+    type: "Ferrari",
+    color: "gold",
+    motor: {
+      serialNumber: 1426,
+      hp: 700,
+    },
+  },
+];
 
 export default function Cars({}: Props) {
   return (
     <div className="p-10">
       <h1 className="font-bold text-4xl mb-5">Cars</h1>
-      <Card car={car} />
+      {cars.map((car: Car) => (
+        <Card car={car} />
+      ))}
     </div>
   );
 }
